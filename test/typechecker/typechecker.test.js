5f9adfbfb6951d0e1c3c8d3d7cdc9690e713e173
AskmienFoodoe <bowen@dragonjiang.com>
2021-03-07 23:42:05 -0500

    Add test harness for typechecker

---
+++ test/typechecker/typechecker.test.js
@@ -0,0 +1,34 @@
+const fs = require('fs');
+const { spawnSync, spawn } = require('child_process');
+const { test, it } = require('@jest/globals');
+
+const cmMakeMsg = '[New bindings added.]';
+
+const getRelativePathOfTests = (dir) => {
+    return fs.readdirSync(dir)
+             .filter(fileName => /^.*\.tig$/.exec(fileName))
+             .map(fileName => dir + fileName);
+
+}
+
+const getScript = (filePath) => {
+    return `CM.make "src/typechecker/sources.cm"; Main.main("${filePath}");`
+}
+
+describe('The textbook tests', () => {
+    const cases = getRelativePathOfTests('test/testcases/');
+
+    test.each(cases)(
+        'parses %s correctly',
+        (testFile) => {
+            const parseResults = spawnSync(`sml`, { input: getScript(testFile) })
+            expect(parseResults.error).toBeFalsy();
+            expect(parseResults.stderr.toString()).toBe('');
+
+            const stdout = parseResults.stdout.toString()
+            expect(stdout.includes(cmMakeMsg)).toBeTruthy();
+            expect(stdout.slice(stdout.indexOf(cmMakeMsg) + cmMakeMsg.length)).toMatchSnapshot();
+            //expect(errorMessagePattern.test(parseResults.stdout.toString())).toBeFalsy();
+        }
+    );
+})
