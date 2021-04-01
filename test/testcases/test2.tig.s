282a0b95260062ddbbe9ffadbe09998c628d3f27
Andy Zhang <az107@cs.duke.edu>
2021-03-31 23:42:36 -0400

    assembly branching fixed

---
+++ test/testcases/test2.tig.s
@@ -0,0 +1,10 @@
+L16:
+addi t136, $0, 10
+move $a0, t136
+addi t138, $0, 0
+move $a1, t138
+jal initArray
+move t134, $v0
+add t134, $0, t134
+j L15
+L15:
---
+++ test/testcases/test8.tig.s
@@ -0,0 +1,16 @@
+L16:
+addi t130, $0, 10
+addi t131, $0, 20
+bgt t130, t131, L12
+ble t130, t131, L13
+L12:
+addi t132, $0, 30
+move t129, t132
+j L14
+L13:
+addi t133, $0, 40
+move t129, t133
+j L14
+add t129, $0, t129
+j L15
+L15:
